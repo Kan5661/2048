ec16dad312d16ec9b70b7e70af89b6ea50995238
Kan5661 <linkan0718@gmail.com>
2022-11-30 12:57:44 -0500

    Can now also combine up ⤴

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ addEventListener('keydown', onPress)
 
 // Global variables
 let BlocksOnBoard = []
-const assets = {
-    1: 'url("./assets/2.png")',
-    2: 'url("./assets/4.png")',
-    3: 'url("./assets/8.png")',
-    4: 'url("./assets/16.png")',
-    5: 'url("./assets/32.png")',
-    6: 'url("./assets/64.png")',
-    7: 'url("./assets/128.png")',
-}
+const assets = [
+        'url("./assets/2.png")',
+        'url("./assets/4.png")',
+        'url("./assets/8.png")',
+        'url("./assets/16.png")',
+        'url("./assets/32.png")',
+        'url("./assets/64.png")',
+        'url("./assets/128.png")',
+        'url("./assets/256.png")',
+        'url("./assets/512.png")',
+        'url("./assets/1024.png")',
+        'url("./assets/2048.png")',
+    ]
 
 //// GAME LOGIC
 
@@ -24,44 +28,65 @@ function random2block() {
         if (boxes[i].style.backgroundImage == '') {emptyBlocks.push(boxes[i])}
     }
     newBlockIndex = Math.floor(Math.random() * emptyBlocks.length)
-    emptyBlocks[newBlockIndex].style.backgroundImage = assets['1']
+    emptyBlocks[newBlockIndex].style.backgroundImage = assets[0]
 }
 
 // WASD + Arrow keys
 function onPress(onPress) {
     if (onPress.key == 'w' || onPress.key == 'ArrowUp') {
-        console.log("up")
+        CombineUp(-1)
     }
     if (onPress.key == 'a' || onPress.key == 'ArrowLeft') {
         console.log('left')
     }
     if (onPress.key == 's' || onPress.key == 'ArrowDown') {
-        Combine(1, 1)
+        CombineDown(1)
     }
     if (onPress.key == 'd' || onPress.key == 'ArrowRight') {
         console.log("right")
     }
 }
 
-function Combine(nextBlock, NB_incre) {
-    let currentBlockIMG, nextBlockIMG
+function CombineDown(NB_incre) {
+    let currentBlockIMG
     for (let i = 0; i < boxes.length; i++) {
-        if ((i % 4 < 3)) {  
+        if (i % 4 < 3) {  
             if (boxes[i].style.backgroundImage != '') {
                 currentBlockIMG = boxes[i].style.backgroundImage
                 
-                if (boxes[i + 1].style.backgroundImage == currentBlockIMG) {
-                    boxes[i + 1].style.backgroundImage = assets[String(Number('1') + 1)]
+                if (boxes[i + NB_incre].style.backgroundImage == currentBlockIMG) {
+                    boxes[i + NB_incre].style.backgroundImage = assets[assets.indexOf(currentBlockIMG) + 1]
                     boxes[i].style.backgroundImage = ''
-                    break
+                    i++
                 }
                     
-                else if (boxes[i + 1].style.backgroundImage == '') {
-                    boxes[i + 1].style.backgroundImage = assets['1']
+                else if (boxes[i + NB_incre].style.backgroundImage == '') {
+                    boxes[i + NB_incre].style.backgroundImage = assets[assets.indexOf(currentBlockIMG)]
                     boxes[i].style.backgroundImage = ''
                 }
-                else if (boxes[i + 1].style.backgroundImage != currentBlockIMG) {
+                
+            }
+        }
+    }
+    random2block()
+}
+
+function CombineUp(NB_incre) {
+    let currentBlockIMG
+    for (let i = 15; i > 0 ; i--) {
+        if (i % 4 > 0) {  
+            if (boxes[i].style.backgroundImage != '') {
+                currentBlockIMG = boxes[i].style.backgroundImage
+                
+                if (boxes[i + NB_incre].style.backgroundImage == currentBlockIMG) {
+                    boxes[i + NB_incre].style.backgroundImage = assets[assets.indexOf(currentBlockIMG) + 1]
+                    boxes[i].style.backgroundImage = ''
+                    i--
+                }
                     
+                else if (boxes[i + NB_incre].style.backgroundImage == '') {
+                    boxes[i + NB_incre].style.backgroundImage = assets[assets.indexOf(currentBlockIMG)]
+                    boxes[i].style.backgroundImage = ''
                 }
                 
             }
@@ -71,5 +96,3 @@ function Combine(nextBlock, NB_incre) {
 }
 
 random2block()
-random2block()
-
