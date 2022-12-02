68df4bc345e190e6b8d3838b4535e7f3fd16984c
Kan5661 <linkan0718@gmail.com>
2022-12-02 15:37:59 -0500

    clear animation and other minor changes

--- app.js
+++ app.js
@@ -2,10 +2,9 @@
 let boxes = document.querySelectorAll(".box")
 let scoreText = document.getElementsByTagName('h2')
 let clearBtn = document.getElementsByClassName('clear')
-console.log(clearBtn)
 
 addEventListener('keydown', onPress) //add event listener to the whole page
-clearBtn[0].addEventListener('click', clearBoard)
+clearBtn[0].addEventListener('click', clearAnimation)
 
 // Global variables
 let RsortedBoxes, score = 0, highScore = 0, win = false, lose = false
@@ -196,13 +195,14 @@ function checkWin() {
 function clearBoard() {
     for (let i = 0; i < boxes.length; i++) {
         boxes[i].style.backgroundImage = ''
-        scoreText[0].innerHTML = `Score: ${score}`
         applyGlow()
     }
     score = 0
+    scoreText[0].innerHTML = `Score: ${score}`
     random2block()
 }
 
+// Functions to sort the boxes nodes list to make it where i can reuse the combine functions
 function reSortBoxes() {
     boxes = [
             RsortedBoxes[0], RsortedBoxes[4], RsortedBoxes[8], RsortedBoxes[12], 
@@ -219,14 +219,64 @@ function sortBoxes() {
             boxes[3], boxes[7], boxes[11], boxes[15]
         ]
 }
+
+// Animations
+
+function clearAnimation(clearAnimation) {
+    let opacity = 1, degree = 0, borderRad = 0, clear = true
+    let animationTimer = setInterval(frame, 20)
+    timer = Date.now()
+    function drawBoxes() {
+        for (let i = 0; i < boxes.length; i++) {
+            boxes[i].style.opacity = opacity
+            boxes[i].style.transform = `rotate(${degree}deg)`
+            boxes[i].style.borderRadius = `${borderRad}%`
+        }
+    }
+    function frame() {
+        if (Date.now() - timer > 4000) {
+            clearInterval(animationTimer)
+            opacity = 1
+            degree = 0
+            borderRad = 0
+            drawBoxes()
+        }
+        else if(Date.now() - timer < 2000) {
+            opacity -= 0.01
+            degree += 3.5
+            borderRad += 0.5
+            drawBoxes()
+
+        }
+        else if(Date.now() - timer > 2000) {
+            if (clear) {
+                clearBoard()
+                clear = false
+            }
+            opacity += 0.01
+            degree -= 3.5
+            borderRad -= 0.5
+            drawBoxes()
+        }
+    }
+    
+}
+
+
 // Calling the functions
 addEventListener('keydown', onPress)
 // initital block when game starts
 random2block()
 
+//// tests
+
 // boxes[0].style.backgroundImage = assets[0]
 // for (let i = 0; i < 11; i++) {
 //     boxes[i+1].style.backgroundImage = assets[i]
 // }
+// applyGlow()
+
+// boxes[1].style.backgroundImage = assets[9]
+// boxes[0].style.backgroundImage = assets[9]
 // applyGlow()
 
